c78863d17038a6b31db1a1e1ad201b5472570110
George Lopez <lopegeor@sheridancollege.ca>
2019-06-18 21:45:29 -0400

    add Deque functions manually, git hub unable to importy Deque from pythonds.basic.deque

--- palindrome.py
+++ palindrome.py
@@ -3,7 +3,50 @@ palindrome.py -- Write the application code here
 """
 # if not already done then install via
 # "pip install pythonds"
-from pythonds.basic.deque import Deque
+#from pythonds.basic.deque import Deque
+class Deque:
+    """
+    manually added Deque function
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        """
+        manually added Deque function
+        """
+        return self.items == []
+
+    def add_front(self, item):
+        """
+        manually added Deque function
+        """
+        self.items.append(item)
+
+    def add_rear(self, item):
+        """
+        manually added Deque function
+        """
+        self.items.insert(0, item)
+
+    def remove_front(self):
+        """
+        manually added Deque function
+        """
+        return self.items.pop()
+
+    def remove_rear(self):
+        """
+        manually added Deque function
+        """
+        return self.items.pop(0)
+
+    def size(self):
+        """
+        manually added Deque function
+        """
+        return len(self.items)
 
 def is_palindrome(inputs):
     """
@@ -21,7 +64,7 @@ def is_palindrome(inputs):
     chardeque = Deque()
 
     for char in inputs:
-        chardeque.addRear(char)
+        chardeque.add_rear(char)
 
     if  inputs == "":
         stillequal = False
@@ -32,8 +75,8 @@ def is_palindrome(inputs):
     elif len(inputs) > 1:
         stillequal = True
         while chardeque.size() > 1 and stillequal:
-            first = chardeque.removeFront()
-            last = chardeque.removeRear()
+            first = chardeque.remove_front()
+            last = chardeque.remove_rear()
             if first != last:
                 stillequal = False
 
